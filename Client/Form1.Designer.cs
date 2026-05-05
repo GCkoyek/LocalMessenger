40c42480562130119f775d045365e51c02d86cc7
koyek <wojtek.andryszak07@gmail.com>
2026-05-06 01:00:13 +0200

    dodanie plikow clienta do github

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System.Windows.Forms;
+
+namespace Clientw
+{
+    partial class ClientForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label statusLabel;
+        private Label label1;
+        private PictureBox pictureBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientForm));
+            this.sendButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            this.sendButton.BackColor = System.Drawing.Color.White;
+            this.sendButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold);
+            this.sendButton.ForeColor = System.Drawing.Color.Black;
+            this.sendButton.Location = new System.Drawing.Point(885, 654);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(101, 53);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Wyślij";
+            this.sendButton.UseVisualStyleBackColor = false;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            this.inputTextBox.BackColor = System.Drawing.Color.White;
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold);
+            this.inputTextBox.ForeColor = System.Drawing.Color.Black;
+            this.inputTextBox.Location = new System.Drawing.Point(20, 654);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(859, 53);
+            this.inputTextBox.TabIndex = 2;
+            this.outputTextBox.BackColor = System.Drawing.Color.White;
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold);
+            this.outputTextBox.ForeColor = System.Drawing.Color.Black;
+            this.outputTextBox.Location = new System.Drawing.Point(20, 50);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(966, 598);
+            this.outputTextBox.TabIndex = 1;
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold);
+            this.statusLabel.ForeColor = System.Drawing.Color.Black;
+            this.statusLabel.Location = new System.Drawing.Point(433, 15);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(191, 24);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Status: Niepołączono";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(960, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1009, 737);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.statusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ClientForm";
+            this.Text = "Klient";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ClientForm_FormClosed);
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ClientForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ClientForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ClientForm_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
